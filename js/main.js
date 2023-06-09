6424ac52f62e2ff995a5e239cfc7cea0f58c284d
AkkiraStar <akkirahilola0908@gmail.com>
2023-06-09 17:28:31 +0500

    Created darkmode with js

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("checkbox").addEventListener("change", function(){
+    document.getElementById("body").classList.toggle('dark');
+    document.getElementById("demo").classList.toggle('changap')
+})
